da01b794390a28d3b77cf3fd185f8e186841cc7d
Alastair Gilles <agilles91@gmail.com>
2020-01-07 17:47:16 +0100

    limit note list view to 20 chars

--- spec/note-list-view-tests.js
+++ spec/note-list-view-tests.js
@@ -1,15 +1,36 @@
-var notesList = new NotesList();
-var notesListView = new NotesListView(notesList);
+(function() {
+  var notesList;
+  var notesListView;
 
-notesList.addNote("string1");
-notesList.addNote("string2");
+  function setUp() {
+    notesList = new NotesList();
+    notesListView = new NotesListView(notesList);
+  }
 
+  function testHTMLString() {
+    setUp();
 
-function testHTMLString(){
+    notesList.addNote("string1");
+    notesList.addNote("string2");
+
     var html = notesListView.getHTMLString();
     var string = "<ul><li>string1</li><li>string2</li></ul>"
 
     assert.isTrue(html === string);
-};
+  };
 
-testHTMLString();
+  function testLongString() {
+    setUp();
+
+    notesList.addNote("Just random things; whatever you want it to be.");
+    notesList.addNote("Random things that are a little different");
+
+    var html = notesListView.getHTMLString();
+    var string = "<ul><li>Just random things; </li><li>Random things that a</li></ul>";
+
+    assert.isTrue(html === string);
+  }
+
+  testHTMLString();
+  testLongString();
+})();
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,12 +1,18 @@
-function NotesListView(notesList) {
+(function(exports) {
+  function NotesListView(notesList) {
     this.notesList = notesList;
-};
+  };
 
 
-NotesListView.prototype.getHTMLString = function() {
+  NotesListView.prototype.getHTMLString = function() {
     var string = "<ul>"
     this.notesList.notes.forEach(note => {
-        string += `<li>${note}</li>`
+      string += `<li>${note.substr(0, 20)}</li>`;
     });
     return string + "</ul>"
-};
+  };
+
+
+
+  exports.NotesListView = NotesListView;
+})(this);
